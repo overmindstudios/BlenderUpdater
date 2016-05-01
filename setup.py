498553f6176da6442bbb9d67cbcd8a971baca138
Tobias Kummer <tobi@overmind-studios.de>
2016-05-01 18:19:52 +0200

    fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.7',
     author='Tobias Kummer for Overmind Studios',
     options={"bdist_esky":
-               {"includes" : [''],
+               {"includes" : ['PyQt4'],
                "freezer_module":"cxfreeze"
 	      }},
     scripts = [Executable('BlenderUpdater.py', gui_only=True, icon="icon.ico")],)
