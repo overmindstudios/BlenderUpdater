c9d3417b7309fda7d9d41484d25d5fb50927f135
Tobias Kummer <tobi@overmind-studios.de>
2016-05-01 18:23:04 +0200

    fixes

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from esky.bdist_esky import Executable
 
 setup(
     name='BlenderUpdater',
-    version='0.7',
+    version='0.8',
     author='Tobias Kummer for Overmind Studios',
     options={"bdist_esky":
-               {"includes" : [''],
+               {"includes" : ['PyQt4'],
                "freezer_module":"cxfreeze"
 	      }},
     scripts = [Executable('BlenderUpdater.py', gui_only=True, icon="icon.ico")],)
