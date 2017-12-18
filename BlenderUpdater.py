d1539580a35b2885dffee76b70a44ada7b190d4c
Tobias Kummer <tobi@overmind-studios.de>
2017-12-18 22:20:57 +0100

    cleaned up stylesheet code

--- BlenderUpdater.py
+++ BlenderUpdater.py
@@ -589,23 +589,26 @@ def main():
 
     dark_palette = QtGui.QPalette()
 
-    dark_palette.setColor(QtGui.QPalette.Window, QtGui.QColor(53, 53, 53))
-    dark_palette.setColor(QtGui.QPalette.WindowText, QtGui.QColor(255, 255, 255))
-    dark_palette.setColor(QtGui.QPalette.Base, QtGui.QColor(25, 25, 25))
-    dark_palette.setColor(QtGui.QPalette.AlternateBase, QtGui.QColor(53, 53, 53))
-    dark_palette.setColor(QtGui.QPalette.ToolTipBase, QtGui.QColor(255, 255, 255))
-    dark_palette.setColor(QtGui.QPalette.ToolTipText, QtGui.QColor(255, 255, 255))
-    dark_palette.setColor(QtGui.QPalette.Text, QtGui.QColor(255, 255, 255))
-    dark_palette.setColor(QtGui.QPalette.Button, QtGui.QColor(53, 53, 53))
-    dark_palette.setColor(QtGui.QPalette.ButtonText, QtGui.QColor(255, 255, 255))
-    dark_palette.setColor(QtGui.QPalette.BrightText, QtGui.QColor(255, 0, 0))
-    dark_palette.setColor(QtGui.QPalette.Link, QtGui.QColor(42, 130, 218))
-    dark_palette.setColor(QtGui.QPalette.Highlight, QtGui.QColor(42, 130, 218))
-    dark_palette.setColor(QtGui.QPalette.HighlightedText, QtGui.QColor(0, 0, 0))
-
+    dark_palette.setColor(dark_palette.Window, QtGui.QColor(53, 53, 53))
+    dark_palette.setColor(dark_palette.WindowText, QtGui.QColor(255, 255, 255))
+    dark_palette.setColor(dark_palette.Base, QtGui.QColor(25, 25, 25))
+    dark_palette.setColor(dark_palette.AlternateBase, QtGui.QColor(53, 53, 53))
+    dark_palette.setColor(dark_palette.ToolTipBase, QtGui.QColor(255, 255, 255))
+    dark_palette.setColor(dark_palette.ToolTipText, QtGui.QColor(255, 255, 255))
+    dark_palette.setColor(dark_palette.Text, QtGui.QColor(255, 255, 255))
+    dark_palette.setColor(dark_palette.Button, QtGui.QColor(53, 53, 53))
+    dark_palette.setColor(dark_palette.ButtonText, QtGui.QColor(255, 255, 255))
+    dark_palette.setColor(dark_palette.BrightText, QtGui.QColor(255, 0, 0))
+    dark_palette.setColor(dark_palette.Link, QtGui.QColor(42, 130, 218))
+    dark_palette.setColor(dark_palette.Highlight, QtGui.QColor(42, 130, 218))
+    dark_palette.setColor(dark_palette.HighlightedText, QtGui.QColor(0, 0, 0))
+    dark_palette.setColor(dark_palette.Disabled, dark_palette.Text,
+                          QtGui.QColor(80, 80, 80))
+    dark_palette.setColor(dark_palette.Disabled, dark_palette.ButtonText,
+                          QtGui.QColor(80, 80, 80))
     app.setPalette(dark_palette)
 
-    app.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
+    app.setStyleSheet("QToolTip {color: #ffffff; background-color: #2a82da; border: 1px solid white;}")
     window.show()
     app.exec_()
 
