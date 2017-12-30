c3d1156a68a23ba3048946c4f85b9a5dc9be1490
Tobias Kummer <tobi@overmind-studios.de>
2017-12-30 14:38:59 +0100

    + beautified status bar + added darkstyle qss and assets + updated screenshot

--- BlenderUpdater.py
+++ BlenderUpdater.py
@@ -583,32 +583,38 @@ class BlenderUpdater(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
 
 
 def main():
-    window = BlenderUpdater()
-
     app.setStyle("Fusion")
 
     dark_palette = QtGui.QPalette()
 
     dark_palette.setColor(dark_palette.Window, QtGui.QColor(53, 53, 53))
     dark_palette.setColor(dark_palette.WindowText, QtGui.QColor(255, 255, 255))
-    dark_palette.setColor(dark_palette.Base, QtGui.QColor(25, 25, 25))
-    dark_palette.setColor(dark_palette.AlternateBase, QtGui.QColor(53, 53, 53))
+    dark_palette.setColor(dark_palette.Disabled, dark_palette.WindowText, QtGui.QColor(127, 127, 127))
+    dark_palette.setColor(dark_palette.Base, QtGui.QColor(42, 42, 42))
+    dark_palette.setColor(dark_palette.AlternateBase, QtGui.QColor(66, 66, 66))
     dark_palette.setColor(dark_palette.ToolTipBase, QtGui.QColor(255, 255, 255))
     dark_palette.setColor(dark_palette.ToolTipText, QtGui.QColor(255, 255, 255))
     dark_palette.setColor(dark_palette.Text, QtGui.QColor(255, 255, 255))
+    dark_palette.setColor(dark_palette.Disabled, dark_palette.Text, QtGui.QColor(127, 127, 127))
+    dark_palette.setColor(dark_palette.Dark, QtGui.QColor(35, 35, 35))
+    dark_palette.setColor(dark_palette.Shadow, QtGui.QColor(20, 20, 20))
+
     dark_palette.setColor(dark_palette.Button, QtGui.QColor(53, 53, 53))
     dark_palette.setColor(dark_palette.ButtonText, QtGui.QColor(255, 255, 255))
     dark_palette.setColor(dark_palette.BrightText, QtGui.QColor(255, 0, 0))
     dark_palette.setColor(dark_palette.Link, QtGui.QColor(42, 130, 218))
     dark_palette.setColor(dark_palette.Highlight, QtGui.QColor(42, 130, 218))
-    dark_palette.setColor(dark_palette.HighlightedText, QtGui.QColor(0, 0, 0))
-    dark_palette.setColor(dark_palette.Disabled, dark_palette.Text,
-                          QtGui.QColor(80, 80, 80))
+    dark_palette.setColor(dark_palette.Disabled, dark_palette.Highlight, QtGui.QColor(80, 80, 80))
+    dark_palette.setColor(dark_palette.HighlightedText, QtGui.QColor(255, 255, 255))
+    dark_palette.setColor(dark_palette.Disabled, dark_palette.HighlightedText, QtGui.QColor(255, 255, 255))
     dark_palette.setColor(dark_palette.Disabled, dark_palette.ButtonText,
-                          QtGui.QColor(80, 80, 80))
+                          QtGui.QColor(127, 127, 127))
     app.setPalette(dark_palette)
 
-    app.setStyleSheet("QToolTip {color: #ffffff; background-color: #2a82da; border: 1px solid white;}")
+    qfdarkstyle = open('darkstyle/darkstyle.qss').read()
+    app.setStyleSheet(qfdarkstyle)
+    window = BlenderUpdater()
+
     window.show()
     app.exec_()
 
--- mainwindow.py
+++ mainwindow.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'mainwindow.ui'
+# Form implementation generated from reading ui file '.\mainwindow.ui'
 #
-# Created by: PyQt5 UI code generator 5.9
+# Created by: PyQt5 UI code generator 5.9.2
 #
 # WARNING! All changes made in this file will be lost!
 
