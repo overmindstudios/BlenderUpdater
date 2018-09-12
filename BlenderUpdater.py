14041bac9870821f035ccb3d42f865d4c004074f
tobi <tobi@overmind-studios.de>
2018-09-12 12:26:41 +0200

    updated license statement

--- BlenderUpdater.py
+++ BlenderUpdater.py
@@ -1,16 +1,17 @@
 """Copyright 2016-2018 Tobias Kummer/Overmind Studios.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-    http://www.apache.org/licenses/LICENSE-2.0
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 from Qt import QtWidgets, QtCore, QtGui
@@ -234,8 +235,8 @@ class BlenderUpdater(QtWidgets.QMainWindow, mainwindow.Ui_MainWindow):
         https://builder.blender.org/download/</span></a></p><p><br/>Developed by Tobias Kummer for \
         <a href="http://www.overmind-studios.de"><span style="text-decoration:underline; color:#2980b9;"> \
         Overmind Studios</span></a></p><p>\
-        Licensed under the <a href="http://www.apache.org/licenses/LICENSE-2.0"><span style=" text-decoration:\
-         underline; color:#2980b9;">Apache 2.0 license</span></a></p><p>Project home: \
+        Licensed under the <a href="https://www.gnu.org/licenses/gpl-3.0-standalone.html"><span style=" text-decoration:\
+         underline; color:#2980b9;">GPL v3 license</span></a></p><p>Project home: \
          <a href="https://overmindstudios.github.io/BlenderUpdater/"><span style=" text-decoration:\
          underline; color:#2980b9;">https://overmindstudios.github.io/BlenderUpdater/</a></p> \
          Application version: ' + appversion + '</body></html> '
@@ -649,7 +650,7 @@ def main():
     dark_palette.setColor(dark_palette.Disabled, dark_palette.HighlightedText, QtGui.QColor(255, 255, 255))
     dark_palette.setColor(dark_palette.Disabled, dark_palette.ButtonText,
                           QtGui.QColor(127, 127, 127))
-    app.setPalette(dark_palette) 
+    app.setPalette(dark_palette)
 
     # qfdarkstyle = open('darkstyle/darkstyle.qss').read()
     # app.setStyleSheet(qfdarkstyle)
