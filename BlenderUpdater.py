612c05e1727db72d53019f07a9e21fbb611fc89e
tobi <tobi@overmind-studios.de>
2018-09-14 11:29:29 +0200

    pep8

--- BlenderUpdater.py
+++ BlenderUpdater.py
@@ -1,4 +1,5 @@
-"""Copyright 2016-2018 Tobias Kummer/Overmind Studios.
+"""
+    Copyright 2016-2018 Tobias Kummer/Overmind Studios.
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
